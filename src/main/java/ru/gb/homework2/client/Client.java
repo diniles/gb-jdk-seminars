cec832fde0e984f81d635d8ff796902f0f0f68c2
Dzmitry Liashko <dzliashko@gmail.com>
2023-10-11 01:17:08 +0300

    Feat: seminar 2 homework class Client

---
+++ src/main/java/ru/gb/homework2/client/Client.java
@@ -0,0 +1,62 @@
+package ru.gb.homework2.client;
+
+import ru.gb.homework2.ServerWindow;
+
+public class Client {
+    private String name;
+    private ClientView clientView;
+    private ServerWindow server;
+    private boolean connected;
+
+    public Client(ClientView clientView, ServerWindow serverWindow) {
+        this.clientView = clientView;
+        this.server = serverWindow;
+    }
+
+    private boolean connectToServer(String name) {
+        this.name = name;
+        if (server.connectUser(this)) {
+            printText("Вы успешно подключились!\n");
+            connected = true;
+            String log = server.getHistory();
+            if (log != null) {
+                printText(log);
+            }
+            return true;
+        } else {
+            printText("Подключение не удалось");
+            return false;
+        }
+    }
+
+    public void sendMessage(String message) {
+        if (connected) {
+            if (!message.isEmpty()) {
+                server.sendMessage(name + ": " + message);
+            }
+        } else {
+            printText("Нет подключения к серверу");
+        }
+    }
+
+    public void serverAnswer(String answer) {
+        printText(answer);
+    }
+
+    public void disconnect() {
+        if (connected) {
+            connected = false;
+            clientView.disconnectFromServer();
+            server.disconnectUser(this);
+            printText("Вы были отключены от сервера!");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void printText(String text) {
+        clientView.showMessage(text);
+    }
+}
