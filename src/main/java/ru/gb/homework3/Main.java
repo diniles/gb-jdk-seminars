da0db92bcd38d329cbbc955a0caeb1d30dce5640
Dzmitry Liashko <dzliashko@gmail.com>
2023-10-18 15:47:46 +0300

    Feat: seminar 3 ArrayComparator

--- src/main/java/ru/gb/homework3/Main.java
+++ src/main/java/ru/gb/homework3/Main.java
@@ -1,11 +1,28 @@
 package ru.gb.homework3;
 
+import static ru.gb.homework3.ArrayComparator.compareArrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Sum: " + Calculator.sum(5, 3));
         System.out.println("Division: " + Calculator.divide(10.0, 2.5));
         System.out.println("Multiply: " + Calculator.multiply(10, 2.5));
         System.out.println("Subtract: " + Calculator.subtract(10, 2));
+
+        Integer[] intArray1 = {1, 2, 3, 4, 5};
+        Integer[] intArray2 = {1, 2, 3, 4, 5};
+        Integer[] intArray3 = {1, 2, 3, 4, 6};
+
+        String[] strArray1 = {"Helloween", "Manowar", "Iron Maiden"};
+        String[] strArray2 = {"Helloween", "Manowar", "Iron Maiden"};
+
+        boolean intArraysEqual = compareArrays(intArray1, intArray2);
+        boolean strArraysEqual = compareArrays(strArray1, strArray2);
+        boolean intArraysNotEqual = compareArrays(intArray1, intArray3);
+
+        System.out.println("intArray1 and intArray2 are the same: " + intArraysEqual); // true
+        System.out.println("strArray1 and strArray2 are the same: " + strArraysEqual); // true
+        System.out.println("intArray1 and intArray3 are the same: " + intArraysNotEqual); // false
     }
 }
 
