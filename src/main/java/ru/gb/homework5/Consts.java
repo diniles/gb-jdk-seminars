69ea50983e2d05bcae0302aff24586fb30bd0fc2
Dzmitry Liashko <dzliashko@gmail.com>
2023-10-30 17:51:06 +0300

    Feat: seminar 5 class Consts

---
+++ src/main/java/ru/gb/homework5/Consts.java
@@ -0,0 +1,8 @@
+package ru.gb.homework5;
+
+public class Consts {
+    public static boolean DEBUG = true;
+    public static int PHILOSOPHERS_COUNT = 5;
+    public static int MAX_WAIT_MS = 1;
+    public static int DINNER_DURATION_IN_MS = 10;
+}
