d1c32e08a7f6418b7fa265c475719b1bc659cc82
Dzmitry Liashko <dzliashko@gmail.com>
2023-10-30 17:30:53 +0300

    Feat: seminar 5 class fork

---
+++ src/main/java/ru/gb/homework5/Fork.java
@@ -0,0 +1,32 @@
+package ru.gb.homework5;
+
+public class Fork {
+    private int id;
+    private boolean availability = true;
+
+    public Fork(int id) {
+        this.id = id;
+    }
+
+    public void take() {
+        long t = java.lang.System.nanoTime();
+        if (Main.DEBUG) {
+            System.out.println(t + ": Fork " + id + " is taken");
+        }
+    }
+
+    public void put() {
+        long t = java.lang.System.nanoTime();
+        if (Main.DEBUG) {
+            System.out.println(t + ": Fork " + id + " is put");
+        }
+    }
+
+    public boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean flag) {
+        availability = flag;
+    }
+}
